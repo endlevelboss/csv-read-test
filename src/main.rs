3a267d63f1898607147db102a7c91cd93c0f8c83
Tony A. Ingebrigtsen <tonyasle@gmail.com>
2024-10-01 13:49:18 +0200

    testing reading dir

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 pub mod myheritage;
 
 fn main() {
-    let d = myheritage::myheritage_loader(19);
-    if d.is_ok() {
-        println!("{:#?}", d);
-    }
-    
+    myheritage::segment_loader(19);    
 }
--- src/myheritage.rs
+++ src/myheritage.rs
@@ -1,56 +1,78 @@
 use csv::Reader;
 use serde::{Serialize, Deserialize};
 use serde_json::to_string;
+use std::{fs, collections::HashMap};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SegmentData {
     id: String,
-    matchname: String,
-    chr: u8,
+    chr: u32,
     start: u32,
     end: u32,
     cm: f32,
     snps: u32,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Segments {
-    data_vector: Vec<SegmentData>,
-}
 
-impl Segments {
-    fn add(&mut self, data: SegmentData) {
-        self.data_vector.push(data);
-    }
-}
+pub fn load_segments_of (file_path: &str, id: &str, selected_chromosome: u32, my_segment_map: &mut HashMap<String, Vec<SegmentData>>) {
+    // let file_path = "data/".to_string() + id + ".csv";
 
-pub fn myheritage_loader (selected_chromosome: u8) -> Result<String, serde_json::Error> {
-    let file_path = "data/tore-mh.csv";
+    println!("reading file: {}", file_path);
     let result = Reader::from_path(file_path);
     
     if result.is_err() {
         println!("Unable to read csv!");
+        return;
     }
 
     let mut myreader = result.unwrap();
 
-    let mut mysegments = Segments{data_vector: Vec::<SegmentData>::new()};
+    
+
     
     for record in myreader.records() {
         let data = record.unwrap();
-        let myid = data.get(0).unwrap().to_string();
+        print!("{:?}", data);
+        let myid = data.get(1).unwrap().to_string();
         let myname = data.get(2).unwrap().to_string();
-        let num: u8 = data.get(3).unwrap().parse().expect("not a number");
-        let mystart: u32 = data.get(4).unwrap().parse().expect("not a number");
-        let myend: u32 = data.get(5).unwrap().parse().expect("not a number");
-        let mycm: f32 = data.get(8).unwrap().parse().expect("not a number");
-        let mysnps: u32 = data.get(9).unwrap().parse().expect("not a number");
+        let num: u32 = data.get(2).unwrap().parse().expect("not a number");
+        let mystart: u32 = data.get(3).unwrap().parse().expect("not a number");
+        let myend: u32 = data.get(4).unwrap().parse().expect("not a number");
+        let mycm: f32 = data.get(7).unwrap().parse().expect("not a number");
+        let mysnps: u32 = data.get(8).unwrap().parse().expect("not a number");
         if num == selected_chromosome {
-            let mydata = SegmentData{id: myid, matchname: myname, chr: num, start: mystart, end: myend, cm: mycm, snps: mysnps};
-            mysegments.add(mydata);
+            let mydata = SegmentData{id: myid, chr: num, start: mystart, end: myend, cm: mycm, snps: mysnps};
+            // mysegments.push(mydata);
+            my_segment_map.entry(id.to_string())
+                .or_insert_with(Vec::new)
+                .push(mydata);
         }        
     }
-    let myjson = to_string(&mysegments);
-    return myjson;
+    
+    // my_segment_map.insert(id.to_string(), mysegments);
 
+}
+
+pub fn segment_loader (selected: u32) {
+    let id1 = "tore";
+    let mut mydata: HashMap<String, Vec<SegmentData>> = HashMap::new();
+    mydata.insert(id1.to_string(), Vec::<SegmentData>::new());
+    
+    let file_dir1 = "data/";
+    // let file_dir2 = "data/".to_string() + id2 + "/";
+
+    for entry in fs::read_dir(file_dir1).unwrap() {
+        let path = entry.unwrap().path();
+        load_segments_of(path.to_str().unwrap(), id1, selected, &mut mydata);
+    }
+           
+
+    // load_segments_of(id1, selected, &mut mydata);
+
+    // load_segments_of(id2, selected, &mut mydata);
+
+    println!("loaded stuff");
+    
+
+    
 }
